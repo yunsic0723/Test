1ab50ca1a6da4057e051bfb7127c0cfb711f92e1
Dasol Park <dasol.park615@gmail.com>
2018-12-05 15:16:27 +0900

    add bodyParser
    test

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
+app.use(bodyParser());
 app.use(cookieParser());
 
 app.listen('3030', function(){
